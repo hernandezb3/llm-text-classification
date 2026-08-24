0a238f4e2cab487c36ab042939eadbbe21e85b2d
Brittney Hernandez <brittney.hernandez476@gmail.com>
2026-08-24 12:50:54 -0400

    incorporate eval metrics

--- local.py
+++ local.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 from huggingface_hub import login
 import transformers
 import torch
-import openpyxl
 import pandas as pd
+import numpy as np
+from sklearn.metrics import confusion_matrix
 
 load_dotenv()
 
@@ -25,7 +26,7 @@ PROMPT = (prompts.loc[prompts.id == "Coding", "prompt"].item() + " " +
           prompts.loc[prompts.id == "Construct", "prompt"].item() + " " +
           prompts.loc[prompts.id == "Prompt1", "prompt"].item())
 
-output_structure = "Format your response as a number surrounded by three ticks. For example, ```1```"
+output_structure = "Format your response as a 0 or 1 surrounded by three ticks. For example, ```1```"
 
 
 # ---- get data ----
@@ -70,4 +71,62 @@ for each_case in range(0, len(DATA)):
         classify_case = int(structured_response.group(1))
     else:
         print(f"no regex pattern detected in response: {response}")
+        classify_case = np.nan
     classifications.append(classify_case)
+
+DATA["code_llm"] = classifications
+
+# ---- evaluate performance ----
+
+codes = pd.DataFrame({"y_true": DATA["code_human"], 
+                      "y_pred": DATA["code_llm"]})
+
+missing_true = codes[codes["y_true"].isna()]
+missing_pred = codes[codes["y_pred"].isna()]
+
+if not missing_true.empty: 
+    print(f"\nThere were {missing_true.shape[0]} missing from code_human. The following cases were dropped from evaluation metric calculations: \n {missing_true}\n ")
+if not missing_pred.empty: 
+    print(f"\nThere were {missing_pred.shape[0]} missing from code_human. The following cases were dropped from evaluation metric calculations. \n {missing_pred}\n ")
+
+codes = codes[codes["y_true"].notna() & codes["y_pred"].notna()]
+
+confusion = confusion_matrix(codes["y_true"], codes["y_pred"])
+print(confusion)
+
+TN, FP, FN, TP = confusion.ravel()
+
+# True negative rate or specificity: proporition of correctly id'd negatives of all that are actually negative
+TNR = TN/(TN+FP) 
+# Negative predictive value: proportion of negatives that are false
+NPV = TN/(TN+FN)
+# False positive rate or fall out: proportion of incorrectly id'd negatives 
+FPR = FP/(TN+FP)
+# False negative rate
+FNR = FN/(TP+FN)
+# False discovery rate: proportion of positives that are false
+FDR = FP/(TP+FP)
+
+# Overall accuracy
+accuracy = (TP+TN)/(TP+FP+FN+TN)
+# Precision or positive predictive value
+precision = TP/(TP+FP)
+# Sensitivity, hit rate, recall, or true positive rate
+recall = TP/(TP+FN)
+# F1 score
+f1 = (2*TP)/(2*TP + FP + FN)
+
+print(f"""\nEVALUATION METRICS \n
+n = {codes.shape[0]}
+n Dropped = {DATA.shape[0] - codes.shape[0]}
+Accuracy = {accuracy}
+Precision = {precision}
+Recall = {recall}
+F1 = {f1}
+True Negative Rate = {TNR}
+Negative Predictive Value = {NPV}
+False Positive Rate = {FPR}
+False Negative Rate = {FNR}
+False Discovery Rate = {FDR}
+""")
+
