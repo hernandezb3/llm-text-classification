907b5a8a9ab9fa1a75161564afa624771fd35da1
Brittney Hernandez <brittney.hernandez476@gmail.com>
2026-09-15 13:48:45 -0400

    debug model call

--- local.py
+++ local.py
@@ -82,7 +82,7 @@ login(token = os.getenv("HF_TOKEN"))
 # ON COLAB
 # meta-llama/Llama-3.2-1B-Instruct (baseline) x
 
-MODEL = "ibm-granite/granite-4.2-8b "
+MODEL = "ibm-granite/granite-4.2-8b"
 TASK = "text-generation"
 TOKENS = 500
 TEMPERATURE = 0.1
